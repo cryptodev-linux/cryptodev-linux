cf70e6f6e716e0d6d78f72caafcc2a84cddefd0d
Joan Bruguera Micó <joanbrugueram@gmail.com>
2023-07-03 00:46:02 +0000

    Move recent Linux version #ifdefs from v6.4 to v6.5
    The latest commits, meant to fix the build on Linux 6.4, are actually
    fixing the build for API changes introduced in the merge window of the
    yet-unreleased Linux 6.5, and actually break the build for Linux 6.4.
    
    In particular, the upstream commits introducing the API changes are the
    following, which are *not* included in the Linux v6.4 tag:
    * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=19c4e618a1bc3d0cad1f04c857be8076cb05bbb2
    * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ca5e863233e8f6acd1792fd85d6bc2729a1b2c10
    
    Change to #ifdef's to v6.5, where they will most likely be included.
    
    Signed-off-by: Joan Bruguera Micó <joanbrugueram@gmail.com>

--- ioctl.c
+++ ioctl.c
@@ -1246,7 +1246,7 @@ static struct ctl_table verbosity_ctl_root[] = {
 	{
 		.procname       = "ioctl",
 		.mode           = 0555,
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0))
 		.child          = verbosity_ctl_dir,
 #endif
 	},
@@ -1269,7 +1269,7 @@ static int __init init_cryptodev(void)
 		return rc;
 	}
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0))
 	verbosity_sysctl_header = register_sysctl_table(verbosity_ctl_root);
 #else
 	verbosity_sysctl_header = register_sysctl(verbosity_ctl_root->procname, verbosity_ctl_dir);
